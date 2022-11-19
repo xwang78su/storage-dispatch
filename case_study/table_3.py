51719a5058c278fa29b3b178f4b51beb0b8ca465
Xiaochu Wang <98920916+xwang78su@users.noreply.github.com>
2022-11-19 14:24:01 -0800

    Update case study

--- case_study/table_3.py
+++ case_study/table_3.py
@@ -10,18 +10,32 @@ with open('table_2.csv','rt') as fh:
     for cell in reader:
         globals()[cell[0]] = varlist[cell[0]] = float(cell[2].strip('$%').replace(',',''))
 
-# TODO implement calculations here based imported globals()
+print("INPUTS")
 for name,value in varlist.items():
-    print(name,'=',value)
+    print(f"{name:10s} = {value}")
 
-Cbase = Dispatch(T=1,a=a_base,c=c,Q0=1-Q,dQ0=-0.05,QT=None,dQT=None,S=1-Q).cost()
-Ccase = Dispatch(T=1,a=a_case,c=c,Q0=1-Q,dQ0=-0.05,QT=None,dQT=None,S=1-Q).cost()
+ask = 0.1
+Q0 = 1-Q
+dQ0 = P/2
+QT = 1-Q + ask
+dQT = None
+S = None
+
+print("PARAMETERS")
+for name in ["Q0","dQ0","QT","dQT","S"]:
+    value = globals()[name]
+    print(f"{name:10s} = {value}")
+
+Cbase = Cmon/768 + ask*Pp
+Ccase = Dispatch(T=1,a=a_case,c=c,Q0=Q0,dQ0=0,QT=QT,dQT=dQT,S=S).cost()
 
 with open('table_3.tex','wt') as tex:
     print("\\begin{tabular}{|l|c|c|c|}",file=tex)
     print("\\hline",file=tex)
     print(" & Base Case & Study Case & Savings \\\\ \\hline",file=tex)
-    print(' & '.join(["Net cost / savings",f"$${Cbase:.2f}/h",f"$${Ccase:.2f}/h",f"{(1-Ccase/Cbase)*100:.1f}%%"]).replace('$$','\\$').replace('%%','\\%'),file=tex)
+    print(' & '.join(["Net cost / savings",f"$${Cbase:.2f}/h",f"$${Ccase:.2f}/h",
+        f"{(1-Ccase/Cbase)*100:.1f}%%"]).replace('$$','\\$').replace('%%','\\%'),file=tex)
     print("\\\\ \\hline",file=tex)
     print("\\end{tabular}",file=tex)
 
+Dispatch(T=1,a=a_case,c=c,Q0=Q0,dQ0=0,QT=QT,dQT=dQT,S=S).plot().savefig("case.png")
